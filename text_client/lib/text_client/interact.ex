b24b8a0dcb2788b166ecb7eba58f89df649200aa
Matt Rhone <kritoke@gmail.com>
2018-03-04 11:21:36 -0600

    Create state module, alias state

--- text_client/lib/text_client/interact.ex
+++ text_client/lib/text_client/interact.ex
@@ -1,18 +1,14 @@
 defmodule TextClient.Interact do
-    
-    defstruct(
-        game_service:   nil,
-        tally:          nil,
-        guessed:        ""
-    )
 
+    alias TextClient.State
+    
     def start() do
         Hangman.new_game()
         |> setup_state()
     end
 
     defp setup_state(game) do
-        %TextClient.Interact{
+        %State{
             game_service:   game,
             tally:          Hangman.tally(game),
         }
