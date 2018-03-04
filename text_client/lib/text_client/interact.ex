76c12efce3083fc26bee3f6a472eaf3894d4771b
Matt Rhone <kritoke@gmail.com>
2018-03-04 11:19:32 -0600

    Start working on initial state code.

--- text_client/lib/text_client/interact.ex
+++ text_client/lib/text_client/interact.ex
@@ -1,6 +1,24 @@
 defmodule TextClient.Interact do
     
+    defstruct(
+        game_service:   nil,
+        tally:          nil,
+        guessed:        ""
+    )
+
     def start() do
         Hangman.new_game()
+        |> setup_state()
+    end
+
+    defp setup_state(game) do
+        %TextClient.Interact{
+            game_service:   game,
+            tally:          Hangman.tally(game),
+        }
+    end
+
+    def play(state) do
+        play(state)
     end
 end
