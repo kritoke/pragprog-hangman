1359a5b51da4b5ae933645a00fc9b707d60fd971
Matt Rhone <kritoke@gmail.com>
2018-03-04 12:06:19 -0600

    fix fn name in mover

--- text_client/lib/text_client/mover.ex
+++ text_client/lib/text_client/mover.ex
@@ -2,7 +2,7 @@ defmodule TextClient.Mover do
 
     alias TextClient.State
 
-    def move(game) do 
+    def make_move(game) do 
         {gs, tally } = Hangman.make_move(game.game_service, game.guess)
         %State{ game | game_service: gs, tally: tally }
     end
