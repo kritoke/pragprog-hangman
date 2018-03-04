245d6a85770e64bce8e258365f87dbee61ba924b
Matt Rhone <kritoke@gmail.com>
2018-03-04 11:47:10 -0600

    display cleanup

--- text_client/lib/text_client/summary.ex
+++ text_client/lib/text_client/summary.ex
@@ -1,6 +1,6 @@
 defmodule TextClient.Summary do
 
-    def display(game = %{ tally: tally}) do
+    def display(%{ tally: tally }) do
         IO.puts [
             "\n",
             "Word so far: #{Enum.join(tally.letters, " ")}\n",
