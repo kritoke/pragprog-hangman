5519a99a171b3277535150ce3c273a9ebea44df4
Matt Rhone <kritoke@gmail.com>
2018-03-04 11:57:59 -0600

    Start adding check input fns)

--- text_client/lib/text_client/prompter.ex
+++ text_client/lib/text_client/prompter.ex
@@ -1,9 +1,30 @@
 defmodule TextClient.Prompter do
 
+    alias TextClient.State
+
     def accept_move(game) do
-        input = IO.gets("Your guess: ")
-        IO.inspect input
-        exit :normal
+        IO.gets("Your guess: ")
+        |> check_input(game)
     end
 
+    defp check_input({:error, reason}, _) do
+        IO.puts("Game ended: #{reason}")
+        exit(:normal)
+    end
+
+    defp check_input({:eof}, _) do
+        IO.puts("Looks like you gave up...")
+        exit(:normal)
+    end
+
+    defp check_input(input, game) do
+        input = String.trim(input)
+        cond do
+            input =~ ~r/\A[a-z]\z/ ->
+                Map.put(game, :guess, input)
+            true ->
+                IO.puts "please enter a single lowercase  letter"
+                accept_move(game)
+        end
+    end
 end
