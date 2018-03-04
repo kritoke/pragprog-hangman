e14dba7823413701e032e9e3074a94dddebe57b1
Matt Rhone <kritoke@gmail.com>
2018-03-04 11:32:50 -0600

    Initial play functions, exit fn.

--- text_client/lib/text_client/player.ex
+++ text_client/lib/text_client/player.ex
@@ -2,7 +2,25 @@ defmodule TextClient.Player do
 
     alias TextClient.State
 
-    def play(%State{}) do
-        
+    def play(%State{tally: %{ game_state: :won}}) do
+        exit_with_message("You WON!") 
+    end
+
+    def play(%State{tally: %{ game_state: :lost}}) do
+        exit_with_message("Sorry, you lost")
+    end
+
+    def play(%State{tally: %{ game_state: :good_guess}}) do
+        IO.puts("Good Guess!")
+    end
+
+    def play(%State{tally: %{ game_state: :bad_guess}}) do
+        IO.puts("Sorry, that isn't in the word")
+    end
+
+
+    defp exit_with_message(msg) do
+        IO.puts(msg)
+        exit(:normal)
     end
 end
