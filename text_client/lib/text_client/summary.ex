69f3e47748be81e863ba6d2cdd78c87f0b0d8812
Matt Rhone <kritoke@gmail.com>
2018-03-11 13:51:07 -0500

    Added display of guessed letters

--- text_client/lib/text_client/summary.ex
+++ text_client/lib/text_client/summary.ex
@@ -5,6 +5,7 @@ defmodule TextClient.Summary do
             "\n",
             "Word so far: #{Enum.join(tally.letters, " ")}\n",
             "Guesses left: #{tally.turns_left}\n",
+            "Letters guessed: #{Enum.join(tally.letters_used, ", ")}\n"
         ]
         game
     end
