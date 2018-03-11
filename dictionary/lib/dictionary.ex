be07cff5c7bc9d6c06e33654b3a42c1a06ab26c7
Matt Rhone <kritoke@gmail.com>
2018-03-11 14:09:15 -0500

    Move word list out into parameter in random word

--- dictionary/lib/dictionary.ex
+++ dictionary/lib/dictionary.ex
@@ -2,7 +2,7 @@ defmodule Dictionary do
 
   alias Dictionary.WordList
 
-  defdelegate random_word(), to: WordList
+  defdelegate random_word(word_list), to: WordList
   defdelegate start(), to: WordList, as: :word_list
 
 end
--- dictionary/lib/dictionary/word_list.ex
+++ dictionary/lib/dictionary/word_list.ex
@@ -1,13 +1,13 @@
 defmodule Dictionary.WordList do
-    def random_word() do
-        word_list()
-        |> Enum.random()
-      end
+    def random_word(word_list) do
+        Enum.random()
+    end
     
-      def word_list do
+    def word_list do
         "../../assets/words.txt"
         |> Path.expand(__DIR__)
         |> File.read!()
         |> String.split(~r/\n/)
-      end
+    end
+    
 end
