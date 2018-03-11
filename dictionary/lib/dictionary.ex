6a6f1607de14a6f8b38c698d6295786481dc0abe
Matt Rhone <kritoke@gmail.com>
2018-03-11 14:06:43 -0500

    Rename fn as start, use word_list fn

--- dictionary/lib/dictionary.ex
+++ dictionary/lib/dictionary.ex
@@ -3,6 +3,6 @@ defmodule Dictionary do
   alias Dictionary.WordList
 
   defdelegate random_word(), to: WordList
-  defdelegate word_list(), to: WordList
+  defdelegate start(), to: WordList, as: :word_list
 
 end
