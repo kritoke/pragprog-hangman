cfdfafe5ea4405d9f0b45f42444f2a3fd6578261
Matthew Rhone <matthew.rhone@outlook.com>
2018-06-09 14:35:18 -0500

    Create supervisor for wordlist.

--- dictionary/lib/dictionary/application.ex
+++ dictionary/lib/dictionary/application.ex
@@ -2,8 +2,19 @@ defmodule Dictionary.Application do
     
     use Application
 
-    def start(_type, _args) do
-        Dictionary.WordList.start_link()
+    def start(_type, _args) do   
+        import Supervisor.Spec
+
+        children = [
+            worker(Dictionary.WordList, []),
+        ]
+
+        options = [
+            name: Dictionary.Supervisor,
+            strategy: :one_for_one,
+        ]
+        
+        Supervisor.start_link(children, options)
     end
     
 end
