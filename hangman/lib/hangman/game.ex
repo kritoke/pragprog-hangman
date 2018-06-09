475413d056c1c85ddebffef14e925dbce6259c4f
Matthew Rhone <matthew.rhone@outlook.com>
2018-06-09 16:30:55 -0500

    move make move back to game, update logic for it.

--- hangman/lib/hangman/game.ex
+++ hangman/lib/hangman/game.ex
@@ -14,15 +14,17 @@ defmodule Hangman.Game do
     end
 
     def new_game() do
-        new_game(Dictionary.start |> Dictionary.random_word)
+        new_game(Dictionary.random_word)
     end
 
     def make_move(game = %{ game_state: state }, _guess) when state in [:won, :lost] do
         game
+        |> return_with_tally()
     end
 
     def make_move(game, guess) do
         accept_move(game, guess, MapSet.member?(game.used, guess))
+        |> return_with_tally()
     end
 
     def tally(game) do
@@ -74,5 +76,7 @@ defmodule Hangman.Game do
 
     defp maybe_won(true),    do: :won
     defp maybe_won(_),       do: :good_guess
+
+    defp return_with_tally(game), do: { game, tally(game) }
 
 end
