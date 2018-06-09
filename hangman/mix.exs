13c9b3f2d5cde514b7a6ced77e754e3db187813b
Matthew Rhone <matthew.rhone@outlook.com>
2018-06-09 17:50:10 -0500

    Made hangman supervisor.

--- hangman/mix.exs
+++ hangman/mix.exs
@@ -14,7 +14,10 @@ defmodule Hangman.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      mod: { Hangman.Application, [] },
+      extra_applications: [
+        :logger
+      ]
     ]
   end
 
