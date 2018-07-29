42a0d38869f854641d3f88389d5775f3f86dba14
Matthew <matthew.rhone@outlook.com>
2018-07-29 12:27:10 -0500

    Added rpc library in new game fn.

--- text_client/lib/text_client/interact.ex
+++ text_client/lib/text_client/interact.ex
@@ -1,9 +1,11 @@
 defmodule TextClient.Interact do
 
+    @hangman_server :hangman@solus
+
     alias TextClient.{Player, State}
     
     def start() do
-        Hangman.new_game()
+        new_game()
         |> setup_state()
         |> Player.play()
     end
@@ -13,6 +15,14 @@ defmodule TextClient.Interact do
             game_service:   game,
             tally:          Hangman.tally(game),
         }
+    end
+
+    defp new_game() do
+        Node.connect(@hangman_server)
+        :rpc.call(@hangman_server, 
+        Hangman,
+        :new_game,
+        [])
     end
 
 end
