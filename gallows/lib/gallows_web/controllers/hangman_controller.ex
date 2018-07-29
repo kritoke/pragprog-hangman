01b6980d68cb752c8c2ca83ffcfaf66ed0e4657f
Matthew <matthew.rhone@outlook.com>
2018-07-29 14:00:21 -0500

    Added create_game functionality to web.

--- gallows/lib/gallows_web/controllers/hangman_controller.ex
+++ gallows/lib/gallows_web/controllers/hangman_controller.ex
@@ -4,4 +4,11 @@ defmodule GallowsWeb.HangmanController do
   def new_game(conn, _params) do
     render conn, "new_game.html"
   end
+
+  def create_game(conn, _params) do
+    game = Hangman.new_game()
+    conn
+    |> put_session(:game, game)
+    |> render("game_field.html")
+  end
 end
